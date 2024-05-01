a4e568ca33e874ebd3d63cdf551171713c52811e
Eugene Abrosimov <sadcitizen@yandex.ru>
2024-05-01 20:13:19 +0500

    add deploy script

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from 'gatsby';
 const config: GatsbyConfig = {
     siteMetadata: {
         title: `sadcitizen.me`,
-        siteUrl: `https://www.yourdomain.tld`
+        siteUrl: `http://sadcitizen.me`
     },
     // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
     // If you use VSCode you can also use the GraphQL plugin
